fc6291f305662db3c03bee67529b46efd52a6b58
Shuflduf <shuflduf@gmail.com>
2025-09-29 12:36:56 -0600

    alifshcnsd';

---
+++ item_tooltips.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+
+func _on_tex_one_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_tex_one_mouse_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_tex_two_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_tex_two_mouse_exited() -> void:
+	pass # Replace with function body.
