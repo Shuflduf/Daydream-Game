c1a2286b346304e5fc6fbd9ff5bc6f890f8159d0
Shuflduf <shuflduf@gmail.com>
2025-09-29 21:59:56 -0600

    fuck yea

--- HUD/hud.gd
+++ HUD/hud.gd
@@ -7,7 +7,7 @@ extends Control
 @onready var transition: ColorRect = $Transition
 
 func _ready() -> void:
-	transition.open(0.5)
+	transition.open()
 
 func set_item(first: bool, id: StringName):
 	$ItemTooltips.set_item(first, id)
