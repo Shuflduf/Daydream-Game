b9aa7ef6996a82257ae0a5d4ab500da1a9de23a0
Shuflduf <shuflduf@gmail.com>
2025-09-29 21:07:35 -0600

    sick fuckking transition part 2

--- Menus/transition.gd
+++ Menus/transition.gd
@@ -2,5 +2,7 @@ extends ColorRect
 
 func open(speed: float = 1.0):
 	while position.y < size.y:
+		$SFX.play()
 		position.y += 32.0
 		await get_tree().create_timer(0.1 / speed).timeout
+	$Finish.play()
